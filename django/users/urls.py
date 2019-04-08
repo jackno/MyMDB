2f8be617564fef6695241e46a47b78e482e35303
Jack Novotny <jacknovotny8@gmail.com>
2019-04-08 12:05:56 -0700

    implement login and logout views

--- django/users/urls.py
+++ django/users/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from . import views
@@ -7,5 +8,11 @@ urlpatterns = [
     path('register',
         views.RegisterView.as_view(),
         name='register'),
+    path('login',
+        auth_views.LoginView.as_view(),
+        name='login'),
+    path('logout',
+        auth_views.LogoutView.as_view(),
+        name='logout'),
 ]
 
