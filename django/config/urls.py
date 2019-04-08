b025f8d5f706080bfa8b708ba4e0787475b298ee
Jack Novotny <jacknovotny8@gmail.com>
2019-04-08 11:49:02 -0700

    add register url pattern

--- django/config/urls.py
+++ django/config/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 import core.urls
+import users.urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include(users.urls, namespace='users')),
     path('', include(core.urls, namespace='core')),
 ]
