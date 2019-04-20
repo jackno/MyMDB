265514b91b07531ab88e0924fcec1f2bfa40881b
Jack Novotny <jacknovotny8@gmail.com>
2019-04-20 14:02:13 +0800

    create update vote view

--- django/core/views.py
+++ django/core/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.views.generic import (
     CreateView,
     DetailView,
     ListView,
+    UpdateView,
 )
 
 from .models import Movie, Vote
@@ -72,5 +74,28 @@ class CreateVote(LoginRequiredMixin, CreateView):
             kwargs = {
                 'pk': movie_id
             })
+        return redirect(to=movie_detail_url)
+
+
+class UpdateVote(LoginRequiredMixin, UpdateView):
+    form_class = VoteForm
+    queryset = Vote.objects.all()
+
+    def get_object(self, queryset=None):
+        vote = super().get_object(queryset)
+        user = self.request.user
+        if vote.user != user:
+            raise PermissionDenied("Cannot change another user's vote")
+        return vote
+
+    def get_success_url(self):
+        movie_id = self.object.movie.id
+        return reverse('core:movie_detail', kwargs={'pk': movie_id})
+
+    def render_to_response(self, context, **response_kwargs):
+        movie_id = context['object'].id
+        movie_detail_url = reverse(
+            'core:movie_detail',
+            kwargs={'pk': movie_id})
         return redirect(to=movie_detail_url)
 
